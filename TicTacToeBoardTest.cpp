0d2dc852d59ceb2ac97f64c652f165b22d03fbee
Mohit Athwani <mohit.athwani@gmail.com>
2017-03-02 22:58:57 -0800

    Added getwinner tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -99,7 +99,7 @@ TEST(TicTacToeBoardTest, getPiece_Row_Valid_Col_Less_Than_0)
 	ASSERT_TRUE(ttt.getPiece(1,-1) == Invalid);
 }
 
-TEST(TicTacToeBoardTest, getHorizontalWinnerTest) {
+TEST(TicTacToeBoardTest, getWinnerTest_Horizontal) {
 	ttt.clearBoard();
 	ASSERT_TRUE(ttt.placePiece(0,0) == X);
 	ASSERT_TRUE(ttt.placePiece(1,0) == O);
@@ -107,4 +107,61 @@ TEST(TicTacToeBoardTest, getHorizontalWinnerTest) {
 	ASSERT_TRUE(ttt.placePiece(1,1) == O);
 	ASSERT_TRUE(ttt.placePiece(0,2) == X);
 	ASSERT_TRUE(ttt.getWinner() == X);
+}
+
+TEST(TicTacToeBoardTest, getWinnerTest_Vertical)
+{
+	ttt.clearBoard();
+	ASSERT_TRUE(ttt.placePiece(0,0) == X);
+	ASSERT_TRUE(ttt.placePiece(0,1) == O);
+	ASSERT_TRUE(ttt.placePiece(1,0) == X);
+	ASSERT_TRUE(ttt.placePiece(0,2) == O);
+	ASSERT_TRUE(ttt.placePiece(2,0) == X);
+	ASSERT_TRUE(ttt.getWinner() == X);
+
+}
+
+TEST(TicTacToeBoardTest, getWinnertest_LeftDiagonal)
+{
+	ttt.clearBoard();
+	ASSERT_TRUE(ttt.placePiece(0,0) == X);
+	ASSERT_TRUE(ttt.placePiece(0,1) == O);
+	ASSERT_TRUE(ttt.placePiece(1,1) == X);
+	ASSERT_TRUE(ttt.placePiece(2,0) == O);
+	ASSERT_TRUE(ttt.placePiece(2,2) == X);
+	ASSERT_TRUE(ttt.getWinner() == X);
+
+}
+
+TEST(TicTacToeBoardTest, getWinnerTest_RightDiagonal)
+{
+	ttt.clearBoard();
+	ASSERT_TRUE(ttt.placePiece(0,2) == X);
+	ASSERT_TRUE(ttt.placePiece(0,1) == O);
+	ASSERT_TRUE(ttt.placePiece(1,1) == X);
+	ASSERT_TRUE(ttt.placePiece(2,1) == O);
+	ASSERT_TRUE(ttt.placePiece(2,0) == X);
+	ASSERT_TRUE(ttt.getWinner() == X);
+}
+
+TEST(TicTacToeBoardTest, getWinnerTest_Invalid)
+{
+	ttt.clearBoard();
+	ASSERT_TRUE(ttt.placePiece(0,2) == X);
+	ASSERT_TRUE(ttt.getWinner() == Blank);
+}
+
+TEST(TicTacToeBoardTest, getWinnerTest_Blank)
+{
+	ttt.clearBoard();
+	ASSERT_TRUE(ttt.placePiece(0,0) == X);
+	ASSERT_TRUE(ttt.placePiece(1,1) == O);
+	ASSERT_TRUE(ttt.placePiece(0,2) == X);
+	ASSERT_TRUE(ttt.placePiece(0,1) == O);
+	ASSERT_TRUE(ttt.placePiece(2,1) == X);
+	ASSERT_TRUE(ttt.placePiece(1,0) == 0);
+	ASSERT_TRUE(ttt.placePiece(1,2) == X);
+	ASSERT_TRUE(ttt.placePiece(2,0) == O);
+	ASSERT_TRUE(ttt.placePiece(2,2) == X);
+	ASSERT_TRUE(ttt.getWinner() == Invalid);
 }
