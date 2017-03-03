5c5dfc612a0b0c0bb6ca006478d900bb0194433a
Mohit Athwani <mohit.athwani@gmail.com>
2017-03-02 21:53:49 -0800

    Renamed and separated tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,44 +20,85 @@ TEST(TicTacToeBoardTest, sanityCheck)
 	ASSERT_TRUE(true);
 
 }
-TEST(TicTacToeBoardTest, placePiece_ValidLocationTest)
+TEST(TicTacToeBoardTest, placePiece_Valid_Location_Test)
 {
 	//TicTacToeBoard ttt;
 	ASSERT_TRUE(ttt.placePiece(0,0) == X);
 	ASSERT_TRUE(ttt.placePiece(1,1) == O);
 }
 
-TEST(TicTacToeBoardTest, placePiece_invalidLocationTest)
+TEST(TicTacToeBoardTest, placePiece_Row_Equals_3_Col_Valid)
 {
 	ASSERT_TRUE(ttt.placePiece(3,1) == Invalid);
+}
+
+TEST(TictacToeBoardTest, placePiece_Row_Greater_Than_3_Col_Valid)
+{
 	ASSERT_TRUE(ttt.placePiece(5, 1) == Invalid);
-	ASSERT_TRUE(ttt.placePiece(1,3) == Invalid);
-	ASSERT_TRUE(ttt.placePiece(1, 5) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePiece_Row_Less_Than_0_Col_Valid)
+{
 	ASSERT_TRUE(ttt.placePiece(-1,1) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePiece_Row_Valid_Col_Equals_3)
+{
+	ASSERT_TRUE(ttt.placePiece(1,3) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePiece_RowValidColGreaterThan3)
+{
+	ASSERT_TRUE(ttt.placePiece(1, 5) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePiece_Row_Valid_Col_Less_Than_0)
+{
 	ASSERT_TRUE(ttt.placePiece(1, -1) == Invalid);
 }
 
-TEST(TicTacToeBoardTest, placePiece_replaceTest)
+TEST(TicTacToeBoardTest, placePiece_Replace_Test)
 {
 	ASSERT_TRUE(ttt.placePiece(0,0) == X);
 }
 
-TEST(TicTacToeBoardTest, getPieceTest_ValidLocationTest)
+TEST(TicTacToeBoardTest, getPieceTest_Valid_Location_Test)
 {
 	ASSERT_TRUE(ttt.getPiece(0,0) == X);
 	ASSERT_TRUE(ttt.getPiece(1,1) == O);
 	ASSERT_TRUE(ttt.getPiece(1,2) == Blank);
 }
 
-TEST(TicTacToeBoardTest, getPiece_InvalidLocationTest)
+TEST(TicTacToeBoardTest, getPiece_Row_Equals_3_Col_Valid)
 {
 	ASSERT_TRUE(ttt.getPiece(3,1) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPiece_Row_Greater_Than_3_Col_Valid)
+{
 	ASSERT_TRUE(ttt.getPiece(4,1) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPiece_Row_Valid_Col_Equals_3)
+{
 	ASSERT_TRUE(ttt.getPiece(1,3) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPiece_Row_Valid_Col_Greater_Than_3)
+{
 	ASSERT_TRUE(ttt.getPiece(1,4) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPiece_Row_Less_Than_0_Col_Valid)
+{
 	ASSERT_TRUE(ttt.getPiece(-1,1) == Invalid);
+}
+
+TEST(TicTacToeBoardTest, getPiece_Row_Valid_Col_Less_Than_0)
+{
 	ASSERT_TRUE(ttt.getPiece(1,-1) == Invalid);
 }
+
 TEST(TicTacToeBoardTest, getWinnerTest) {
 	ASSERT_TRUE(ttt.placePiece(1,0) == O);
 	ASSERT_TRUE(ttt.placePiece(0,1) == X);
