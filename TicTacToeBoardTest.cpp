93eee37bc5faa0ade61b4068315567b550cf28d7
Mohit Athwani <mohit.athwani@gmail.com>
2017-03-02 20:58:46 -0800

    Testing all branches in placePiece

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -29,8 +29,12 @@ TEST(TicTacToeBoardTest, placePiece_ValidLocationTest)
 
 TEST(TicTacToeBoardTest, placePiece_invalidLocationTest)
 {
-	ASSERT_TRUE(ttt.placePiece(3,3) == Invalid);
-	ASSERT_TRUE(ttt.placePiece(-1,-1) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(3,1) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(5, 1) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(1,3) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(1, 5) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(-1,1) == Invalid);
+	ASSERT_TRUE(ttt.placePiece(1, -1) == Invalid);
 }
 
 TEST(TicTacToeBoardTest, placePiece_replaceTest)
@@ -38,13 +42,19 @@ TEST(TicTacToeBoardTest, placePiece_replaceTest)
 	ASSERT_TRUE(ttt.placePiece(0,0) == X);
 }
 
-TEST(TicTacToeBoardTest, getPieceTest) {
+TEST(TicTacToeBoardTest, getPieceTest_ValidLocationTest)
+{
 	ASSERT_TRUE(ttt.getPiece(0,0) == X);
 	ASSERT_TRUE(ttt.getPiece(1,1) == O);
-	ASSERT_TRUE(ttt.getPiece(3,3) == Invalid);
 	ASSERT_TRUE(ttt.getPiece(1,2) == Blank);
 }
 
+TEST(TicTacToeBoardTest, getPiece_InvalidLocationTest)
+{
+	ASSERT_TRUE(ttt.getPiece(3,3) == Invalid);
+	ASSERT_TRUE(ttt.getPiece(-1,-3) == Invalid);
+
+}
 TEST(TicTacToeBoardTest, getWinnerTest) {
 	ASSERT_TRUE(ttt.placePiece(1,0) == X);
 	ASSERT_TRUE(ttt.placePiece(0,1) == X);
